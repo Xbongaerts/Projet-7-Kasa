7bf3c6e7a9aec16f8f1ddbdd604ce01e6740e9fb
Xbongaerts <okrim77@gmail.com>
2023-03-22 13:20:01 +0100

    up app

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -6,6 +6,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
 	{
+		path: "/",
+		element: <Home />
+	},
+	{
 		path: "/Kasa/",
 		element: <Home />
 	},
